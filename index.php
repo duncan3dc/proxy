3b628f12d13cf4b761bd4ffad30105f448730623
Craig Duncan <git@duncanc.co.uk>
2015-06-28 13:23:18 +0100

    Create a basic script to handle the requests

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+require __DIR__ . "/vendor/autoload.php";
+
+$client = new Client;
+
+$url = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+
+$request = new Request($_SERVER["REQUEST_METHOD"], $url);
+
+$response = $client->send($request);
+
+echo $response->getBody();
