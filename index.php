cd6bd04950db75831c9e726de4a4cfae6bce9ffa
Craig Duncan <git@duncanc.co.uk>
2015-12-19 15:39:12 +0000

    Don't convert http errors into exceptions

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ use GuzzleHttp\Psr7\Request;
 
 require __DIR__ . "/vendor/autoload.php";
 
-$client = new Client;
+$client = new Client([
+    "http_errors"   =>  false,
+]);
 
 $url = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 
